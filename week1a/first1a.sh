ff24c27e5abf0396bdf1b226b2687aeaf04cf546
AMasanka <asanka1113@yahoo.com>
2021-08-15 03:17:57 +0000

    week1a scripts

---
+++ week1a/first1a.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hellow There"
+echo "My name is A.M Asanka Silva"
+exit 22
---
+++ week1a/second1a.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Hellow There!" #prints Hellow There!
+echo "It's good to see you now $1!"
+exit 0
